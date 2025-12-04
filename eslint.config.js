d44182cc17739bcb6d37de54a3c9064c2d65e8d6
Daniel-Devops <76241710+delzilwilfred@users.noreply.github.com>
2025-12-04 11:40:40 +0530

    Refactor ESLint configuration to object format

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,9 @@
-module.exports = [
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
-    },
-    rules: {
-      semi: "error",
-      quotes: ["error", "single"],
-      "no-unused-vars": "warn",
-    },
-  },
-];
+module.exports = {
+  root: true,
+  env: { browser: true, es2021: true },
+  rules: {
+    semi: 'error',
+    quotes: ['error', 'single'],
+    'no-unused-vars': 'warn'
+  }
+};
