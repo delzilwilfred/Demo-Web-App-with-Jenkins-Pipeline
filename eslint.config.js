1b30aa4cb6ae56658884704e966233fe8496067a
Daniel-Devops <76241710+delzilwilfred@users.noreply.github.com>
2025-12-04 11:42:35 +0530

    Refactor ESLint config to use export default syntax

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,18 @@
-module.exports = {
-  root: true,
-  env: { browser: true, es2021: true },
-  rules: {
-    semi: 'error',
-    quotes: ['error', 'single'],
-    'no-unused-vars': 'warn'
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly"
+      }
+    },
+    rules: {
+      semi: "error",
+      quotes: ["error", "single"],
+      "no-unused-vars": "warn"
+    }
   }
-};
+];
